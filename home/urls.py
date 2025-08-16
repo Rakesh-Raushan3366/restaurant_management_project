1b977dbf6f0dde94ca7639f15a286de5bf5456f9
Rakesh <Rakeshgnit3366@gmail.com>
2025-08-16 15:19:48 +0530

    first commit

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('', home, name='home'),  # homepage
+    path('restaurant/', restaurant_page, name='restaurant_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def restaurant_page(request):
+    return render(request, 'restaurant.html', context)
